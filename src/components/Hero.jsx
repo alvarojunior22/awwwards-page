43c9a7063d484515499070631d1bd9210c8b3aa2
alvaro ariza <211169975+alvarojunior22@users.noreply.github.com>
2026-01-08 17:30:12 -0500

    feat: enhance video handling and add new content to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,10 +15,12 @@ const Hero = () => {
    setLoadedVideos(prev => prev + 1)
  }
 
+  const upcomingVideoLoad = (currentIndex % totalVideos) + 1
+  
   const handleMinivideoClik = () => {
     setHasCliked(true)
     
-    setCurrentIndex(prev => prev + 1)
+    setCurrentIndex(upcomingVideoLoad)
   }
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`
@@ -28,11 +30,11 @@ const Hero = () => {
     <div className='relative h-dvh w-screen overflow-x-hidden'>
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
-          <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+          <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg opacity-0 scale-90 transition-all duration-300 ease-out hover:opacity-100 hover:scale-100 '>
             <div onClick={handleMinivideoClik} className='origin-center'>
               <video
                 ref={nextVdRef}
-                src={getVideoSrc((currentIndex % totalVideos) + 1)}
+                src={getVideoSrc(upcomingVideoLoad)}
                 loop
                 muted
                 id='current-video'
@@ -40,6 +42,31 @@ const Hero = () => {
                 onLoadedData={handleVideoLoad}
               />
             </div>
+          </div>
+          <video
+            ref={nextVdRef}
+            src={getVideoSrc(currentIndex)} loop
+            muted
+            id='next-video'
+            className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+          className='absolute left-0 top-0 size-full object-cover object-center'/>
+        </div>
+        <h1 className='special-font hero-heading absolute bottom-5 right-5  text-blue-75'>
+          G<b>a</b>ming
+        </h1>
+        <div className='absolute left-0 top-0 z-40 size-full'>
+          <div className='mt-24 px-5 sm:px-10'>
+            <h1 className='special-font hero-heading text-blue-100'>
+              redifi<b>n</b>e
+            </h1>
+            <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>Enter the metagame Layer <br />Unleash the Play economi</p>
           </div>
         </div>
       </div>
